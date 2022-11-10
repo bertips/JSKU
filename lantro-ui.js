dccd2f6dda9aa4578fdeb65db5074fa562793115
Bertips.com <62259363+bertips@users.noreply.github.com>
2022-11-11 02:24:37 +0700

    Update lantro-ui.js

--- lantro-ui.js
+++ lantro-ui.js
@@ -12,7 +12,7 @@ return false
 } else {
 $('body').append('<style>body{background:#000!important;overflow:hidden}#peringatan span{font-size:50px}#peringatan{z-index:999999999;position:fixed;top:0;right:0;left:0;padding:20% 0;height:100%;text-align:center;background:rgba(248,40,40,0.97);color:#fff}}</style><div id="peringatan"><h4>Activate Template</h4><p>Lantro UI is not activated please purchase license to continue using Lantro UI!</p><span id="aktivasi">'+second+'</span></div>');
 setInterval(function () {
-second <= 1 ? window.location.href = "https://www.bertips.com/" : $('#aktivasi').text(--second);
+second <= 1 ? window.location.href = "https://www.protemplates.in" : $('#aktivasi').text(--second);
 }, 1e3)
 }
 });
